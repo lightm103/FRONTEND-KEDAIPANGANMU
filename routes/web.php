785ffd49f7a5338a54b3514a490dec51257de000
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2024-05-13 15:04:01 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+//use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('dashboard');
+});*/
+
+//sample routes
+//Route::get('/', [HomeController::class, 'index'])->name('index');
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/', 'dashboard');
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
+
+// authentication reference
+https://www.allphptricks.com/laravel-custom-user-registration-and-login-tutorial/
